86090f175f3c27ab2fc4d46be2b9f2eaf71a1f1b
tothanhtin123 <tothanhtin123@gmail.com>
2024-03-02 09:43:57 +0700

    feat: add config service

--- wishlist-of-products-be/src/app.module.ts
+++ wishlist-of-products-be/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SharedModule } from './modules/shared/shared.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    SharedModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- wishlist-of-products-be/src/main.ts
+++ wishlist-of-products-be/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SharedModule } from './modules/shared/shared.module';
+import { CommonConfigService } from './modules/shared/common-config/common-config.service';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.select(SharedModule).get(CommonConfigService);
+  const port = config.appConfig.port || 8000;
+
+  await app.listen(port,()=>{
+    Logger.log(`This app is running on port: ${port}`);
+  });
 }
 bootstrap();
