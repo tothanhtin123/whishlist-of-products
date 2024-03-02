db3c6ded50735b895f24cb8b7fafa446e287ac9f
tothanhtin123 <tothanhtin123@gmail.com>
2024-03-02 09:57:14 +0700

    feat: connect to db

--- wishlist-of-products-be/src/app.module.ts
+++ wishlist-of-products-be/src/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SharedModule } from './modules/shared/shared.module';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CommonConfigService } from './modules/shared/common-config/common-config.service';
 
 @Module({
   imports: [
@@ -10,6 +12,20 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
       envFilePath: '.env',
     }),
+    MongooseModule.forRootAsync({
+			useFactory: (configService: CommonConfigService) => ({
+				uri: configService.dbConfig.uri,
+        //check connection
+        connectionFactory:(connection) => {
+          connection.on('connected', () => {
+            console.log('The connection to mongo db is successful');
+         });
+         connection._events.connected();
+         return connection;
+        }
+			}),
+			inject: [CommonConfigService],
+		}),
     SharedModule,
   ],
   controllers: [AppController],
